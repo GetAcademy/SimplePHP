266f404c11bec0f60c7288ebde5c295e51e7c505
GET Academy <terje@kolderup.net>
2021-04-26 14:02:31 +0200

    Initial commit

---
+++ 03_index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hei</h1>
+    <?php
+    if (array_key_exists("a",$_GET) && array_key_exists("b",$_GET)) {
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+        echo $a + $b;
+    } else {
+        echo "<span style=\"color: red\">Angi URL-parametre, a og b</span>";
+    }
+    ?>
+</body>
+
+</html>
