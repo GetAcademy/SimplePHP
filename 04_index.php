58657036c474a96b7f83f51c9a0b85585e5f8d33
GET Academy <terje@kolderup.net>
2021-04-26 14:07:38 +0200

    Create 04_index.php

---
+++ 04_index.php
@@ -0,0 +1,14 @@
+<?php
+$returnValue = new stdClass();
+if (array_key_exists("a", $_GET) && array_key_exists("b", $_GET)) {
+    $a = (int)$_GET['a'];
+    $b = (int)$_GET['b'];    
+    $returnValue->a = $a;
+    $returnValue->b = $b;
+    $returnValue->sum = $a + $b;
+} else {
+    $returnValue->errorMessage = "Angi URL-parametre, a og b";
+    $returnValue->isError = true;
+}
+echo json_encode($returnValue);
+?>
